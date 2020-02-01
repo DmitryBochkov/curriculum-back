ae26870b1b38416d168c226dbd717716c8233259
Dmitry Bochkov <dmitry.bochkov@gmail.com>
2020-02-01 02:59:50 +0200

    create curriculum-back, first commit

---
+++ server/api/curricula.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', function (req, res) {
+  res.send('Hello curricula!')
+})
+
+router.post('/', function (req, res) {
+  res.send('Got a POST request')
+})
+
+router.get('/:id', function (req, res) {
+  res.send(req.params)
+})
+
+router.patch('/:id', function (req, res) {
+  res.send('Got a POST request')
+})
+
+router.delete('/:id', function (req, res) {
+  res.send('Got a POST request')
+})
+
+module.exports = router
---
+++ server/api/index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const router = express.Router()
+const curricula = require('./curricula')
+
+router.use('/curricula', curricula)
+
+module.exports = router
